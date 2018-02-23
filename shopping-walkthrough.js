05f565f9165d955a37b4e8149da2512cc98c9dda
Joshua Hutchinson <jthutch94@gmail.com>
2018-02-23 11:28:01 -0500

    shopping-list-walkthrough1

---
+++ shopping-walkthrough.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function renderShoppingList(){
+    console.log('renderShoppingList ran');
+}
+
+function addShoppingList(){
+    console.log('addShoppingList ran';)
+}
+
+function checkListItems(){
+    console.log('checkListItem ran';)
+}
+
+function deleteListItem(){
+    console.log('deleteListItem ran';)
+}
+
+function handleShoppingList(){
+    
+}
