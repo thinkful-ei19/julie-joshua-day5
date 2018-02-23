bc0337ea4e2cc54c459f1724de49eda53930e5c2
Julie K <julieskim160@gmail.com>
2018-02-23 09:04:01 -0800

    generate item dynamically

--- shopping-walkthrough.js
+++ shopping-walkthrough.js
@@ -7,15 +7,26 @@ const STORE = [
     {name: 'bread', checked: false}
 ];
 
+function generateItemElement(item, itemIndex, template) {
+    return `
+    <li class="js-item-index-element" data-item-index="${itemIndex}">
+    <span class="shopping-item js-shopping-item ${item.checked ? "shopping-item__checked" : ''}">${item.name}</span>
+    <div class="shopping-item-controls">
+      <button class="shopping-item-toggle js-item-toggle">
+          <span class="button-label">check</span>
+      </button>
+      <button class="shopping-item-delete js-item-delete">
+          <span class="button-label">delete</span>
+      </button>
+    </div>
+  </li>`; 
+}
+
 function generateShoppingItemsString(shoppingList){
     console.log("Generating shopping list element");
 
-    return `
-    <li>apples</li>
-    <li>oranges</li>
-    <li>milk</li>
-    <li>bread</li>
-    `;
+    const items = shoppingList.map((item, index) => generateItemElement(item,index));
+    return items.join("");
 }
 
 function renderShoppingList(){
